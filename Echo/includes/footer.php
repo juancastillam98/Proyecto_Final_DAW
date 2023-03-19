43b516607aa1a39d6abb55f984c4855f9270229f
juancastillam98 <castillamariscal.juan@gmail.com>
2023-03-19 21:00:29 +0100

    Agregado el proyecto

---
+++ Echo/includes/footer.php
@@ -0,0 +1,7 @@
+<div class="pie-container">
+    <div class="logo"></div>
+    <div class="politicas">
+        <a href="./contact/cookies.html" class="politicas">Políticas de Cookies</a>
+        <a href="./contact/privacidad.html" class="politicas">Políticas de privacidad</a>
+    </div>
+</div>
