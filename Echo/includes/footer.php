5d1a89a3ee26986eb5109228fd3e443f4bd821e8
juancastillam98 <89079551+juancastillam98@users.noreply.github.com>
2023-05-18 14:00:38 +0200

    Delete Echo directory

--- Echo/includes/footer.php
+++
@@ -1,7 +0,0 @@
-<div class="pie-container">
-    <div class="logo"></div>
-    <div class="politicas">
-        <a href="./contact/cookies.html" class="politicas">Políticas de Cookies</a>
-        <a href="./contact/privacidad.html" class="politicas">Políticas de privacidad</a>
-    </div>
-</div>
